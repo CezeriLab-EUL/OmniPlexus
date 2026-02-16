4b1881442091e46b0a37e1353b1f934bd576f9b5
Dunamis Olukayode <callmestiless@gmail.com>
2026-02-16 15:00:09 +0200

    add command registry header

---
+++ include/smartdrive/registry/CommandRegistry.h
@@ -0,0 +1,66 @@
+//
+// Created by dunamis on 16/02/2026.
+//
+
+#ifndef SMARTDRIVE_COMMANDREGISTRY_H
+#define SMARTDRIVE_COMMANDREGISTRY_H
+
+#include <cstdint>
+#include "../core/ValueSource.h"
+
+#ifndef EMBEDDED_BUILD
+#include <map>
+#include <string>
+#include <vector>
+
+struct ParamInfo {
+    std::string name;
+    ValueType type;
+    bool required;
+    std::string description;
+    size_t maxLength = 0;
+    std::string defaultValue;
+};
+
+struct CommandMeta {
+    uint16_t commandType;
+    std::string name;
+    std::string description;
+    std::vector<ParamInfo> params;
+};
+
+class CommandRegistry {
+private:
+    std::map<uint16_t, CommandMeta> commands;
+
+public:
+    void registerCommand(CommandMeta meta) {
+        commands[meta.commandType] = meta;
+    }
+
+    const CommandMeta* getCommandInfo(uint16_t commandType) const {
+        auto it = commands.find(commandType);
+        return it != commands.end() ? &it->second : nullptr;
+    }
+
+    const CommandMeta* findByName(const std::string& name) const {
+        for (const auto& [id, meta]: commands) {
+            if (meta.name == name) return &meta;
+        }
+        return nullptr;
+    }
+
+    std::vector<const CommandMeta*> getAllCommands() const {
+        std::vector<const CommandMeta*> result;
+        result.reserve(commands.size());
+        for (const auto& [id, meta]: commands) {
+            result.push_back(&meta);
+        }
+        return result;
+    }
+
+    void initialize();
+};
+#endif
+
+#endif //SMARTDRIVE_COMMANDREGISTRY_H
